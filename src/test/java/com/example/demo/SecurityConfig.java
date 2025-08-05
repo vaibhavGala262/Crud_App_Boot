c742f40171f3628037bf81b0b5f3fdacb2ee22ad
Vaibhav Gala <vaibhavgala262@gmail.com>
2025-08-05 21:53:14 +0530

    Added Login with Google and Github

---
+++ src/test/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+
+                )
+                .formLogin(Customizer.withDefaults()
+                )
+                .oauth2Login(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+
+}
